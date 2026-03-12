7b2e001e4fc48cafb10b8cbee42a04b9da7de888
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2026-03-12 15:16:25 -0300

    first commit

---
+++ src/main/java/com/example/progress_study_backend/ProgressStudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.progress_study_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressStudyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressStudyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progress_study_backend/ProgressStudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progress_study_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressStudyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
